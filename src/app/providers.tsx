848737a4981c2650a206b3a8fd9042f76b31b408
Sumin <robolindasoo@gmail.com>
2024-08-26 00:05:20 +0900

    :sparkles: 서버사이드 요청을 발생시키기 위해 dehydratedQueries 추가

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,17 +2,22 @@
 
 import { MSWProvider } from '@/widgets/msw';
 import { NextUIProvider } from '@nextui-org/system';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import { MswDevtools } from '@/widgets/msw/ui/msw-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { queryClientConfig } from '@/shared/lib/dehydrated-query-client';
 
 const Providers: React.FC<PropsWithChildren> = (props) => {
   const { children } = props;
+  const [queryClient] = useState(() => new QueryClient(queryClientConfig));
 
   return (
-    <MSWProvider>
-      <NextUIProvider>{children}</NextUIProvider>
-      <MswDevtools enabled={process.env.NEXT_PUBLIC_DEV_MODE === 'msw'} />
-    </MSWProvider>
+    <QueryClientProvider client={queryClient}>
+      <MSWProvider>
+        <NextUIProvider>{children}</NextUIProvider>
+        <MswDevtools enabled={process.env.NEXT_PUBLIC_DEV_MODE === 'msw'} />
+      </MSWProvider>
+    </QueryClientProvider>
   );
 };
 
---
+++ src/shared/lib/dehydrated-query-client.tsx
@@ -0,0 +1,54 @@
+import { cache } from 'react';
+import {
+  dehydrate,
+  MutationCache,
+  QueryClient,
+  QueryClientConfig,
+  QueryKey,
+} from '@tanstack/react-query';
+
+const isSameQueryKey = (q1: QueryKey, q2: QueryKey) => {
+  return JSON.stringify(q1) === JSON.stringify(q2);
+};
+
+const queryClientConfig: QueryClientConfig = {
+  mutationCache: new MutationCache({
+    onSuccess: (_data, _variables, _context, mutation) => {
+      const queryClient = getQueryClient();
+      void queryClient.invalidateQueries({
+        queryKey: mutation.options.mutationKey,
+        exact: false,
+      });
+    },
+  }),
+};
+
+const getQueryClient = cache(() => new QueryClient(queryClientConfig));
+
+const getDehydratedQueryClientByQueries = async <
+  PrefetchQueries extends {
+    queryKey: QueryKey;
+    queryFn: () => ReturnType<PrefetchQueries['queryFn']>;
+  },
+>(
+  prefetchQueries: PrefetchQueries[]
+) => {
+  const queryClient = getQueryClient();
+
+  await Promise.all(
+    prefetchQueries.map(async ({ queryKey, queryFn }) =>
+      queryClient.prefetchQuery({ queryKey, queryFn })
+    )
+  );
+
+  const { queries } = dehydrate(queryClient, {
+    shouldDehydrateQuery: (query) =>
+      prefetchQueries.some((prefetchQuery) =>
+        isSameQueryKey(prefetchQuery.queryKey, query.queryKey)
+      ),
+  });
+
+  return queries.filter(({ state }) => state.status !== 'error');
+};
+
+export { getQueryClient, queryClientConfig, getDehydratedQueryClientByQueries };
