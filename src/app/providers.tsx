61348e713f8cb81fbe5ce7c7769e513ed70146ed
Sumin <robolindasoo@gmail.com>
2024-08-25 18:33:34 +0900

    :heavy_plus_sign: nextui 설정

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { MSWProvider } from '@/widgets/msw';
+import { NextUIProvider } from '@nextui-org/system';
 import { PropsWithChildren } from 'react';
 
 const Providers: React.FC<PropsWithChildren> = (props) => {
   const { children } = props;
 
-  return <MSWProvider>{children}</MSWProvider>;
+  return (
+    <MSWProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </MSWProvider>
+  );
 };
 
 export default Providers;
