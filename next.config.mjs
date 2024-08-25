9247a37f24acc45e826efa254d84370d5ff64903
Sumin <robolindasoo@gmail.com>
2024-08-25 16:52:00 +0900

    :wrench: 서버사이드 msw를 활성화하기 위한 next config 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      /**
+       * Setting `resolve.alias` to `false` will tell webpack to ignore a module.
+       * `msw/node` is a server-only module that exports methods not available in the `browser`.
+       *
+       * @see https://github.com/mswjs/msw/issues/1801#issuecomment-1794145119
+       */
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'msw/node': false,
+      };
+    }
+
+    return config;
+  },
+  experimental: { instrumentationHook: !isProduction },
+};
 
 export default nextConfig;
