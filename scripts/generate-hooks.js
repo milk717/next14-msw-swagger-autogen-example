213cc03f3f10f82bdc1fe54b005651d402c2e0e5
Sumin <robolindasoo@gmail.com>
2024-08-31 15:03:49 +0900

    :recycle: refactor script

--- scripts/generate-hooks.js
+++ scripts/generate-hooks.js
@@ -1,14 +1,30 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
+import minimist from 'minimist';
 import { generateApi } from 'swagger-typescript-api';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const argv = minimist(process.argv.slice(2), {
+  string: ['hook'],
+  alias: {
+    h: 'hook',
+  },
+});
+
+const { hook } = argv;
+
+if (!hook) {
+  console.error('❗️ Error: Please provide the hook type.');
+  console.error('Usage: node generate-hooks.js --hook <queries|mutations>');
+  process.exit(1);
+}
+
 generateApi({
   input: path.resolve(__dirname, './swagger/todo-api.yml'),
-  templates: path.resolve(__dirname, './templates/queries'),
+  templates: path.resolve(__dirname, `./templates/${hook}`),
   generateClient: true,
   generateUnionEnums: true,
   cleanOutput: false,
@@ -33,7 +49,7 @@ generateApi({
         __dirname,
         `../src/entities/${fileName.toLowerCase()}/api`
       );
-      const fileOutputPath = path.join(directoryPath, 'queries.ts');
+      const fileOutputPath = path.join(directoryPath, `${hook}.ts`);
 
       fs.mkdir(directoryPath, { recursive: true }, (err) => {
         if (err) {
